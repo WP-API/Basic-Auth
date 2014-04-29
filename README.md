1d1f319bf2dd9f701b5bee9d57debfc3eadb42b3
Ryan McCue <me@ryanmccue.info>
2014-04-29 11:44:54 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# Basic Authentication handler
+This plugin adds Basic Authentication to a WordPress site.
+
+Note that this plugin requires sending your username and password with every
+request, and should only be used for development and testing. We strongly
+recommend using the [OAuth 1.0a][oauth] authentication handler for production.
+
+## Installing
+1. Download the plugin into your plugins directory
+2. Enable in the WordPress admin
+
+## Using
+This plugin adds support for Basic Authentication, as specified in [RFC2617][].
+Most HTTP clients will allow you to use this authentication natively. Some
+examples are listed below.
+
+### cURL
+
+```sh
+curl --user admin:password http://example.com/wp-json/
+```
+
+### WP_Http
+
+```php
+$args = array(
+	'headers' => array(
+		'Authentication' => base64_encode( $username . ':' . $password ),
+	),
+);
+```
+
+[oauth]: https://github.com/WP-API/OAuth1
+[RFC2617]: https://tools.ietf.org/html/rfc2617
---
+++ basic-auth.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Plugin Name: JSON Basic Authentication
+ * Description: Basic Authentication handler for the JSON API, used for development and debugging purposes
+ * Author: WordPress API Team
+ * Author URI: https://github.com/WP-API
+ * Version: 0.1
+ * Plugin URI: https://github.com/WP-API/Basic-Auth
+ */
+
+function json_basic_auth_handler( $user ) {
+	// Don't authenticate twice
+	if ( ! empty( $user ) ) {
+		return $user;
+	}
+
+	// Check that we're trying to authenticate
+	if ( !isset( $_SERVER['PHP_AUTH_USER'] ) ) {
+		return false;
+	}
+
+	$username = $_SERVER['PHP_AUTH_USER'];
+	$password = $_SERVER['PHP_AUTH_PW'];
+
+	global $wp_json_basic_auth_error;
+
+	$user = wp_authenticate( $username, $password );
+
+	if ( is_wp_error( $user ) ) {
+		$wp_json_basic_auth_error = $user;
+		return null;
+	}
+
+	return $user->ID;
+}
+add_filter( 'determine_current_user', 'json_basic_auth_handler' );
+
+function json_basic_auth_error( $error ) {
+	// Passthrough other errors
+	if ( ! empty( $error ) ) {
+		return $error;
+	}
+
+	global $wp_json_basic_auth_error;
+
+	// If we don't have an error, passthrough the null
+	if ( empty( $wp_json_basic_auth_error ) ) {
+		return $error;
+	}
+
+	// We have an error! Return it.
+	return $wp_json_basic_auth_error;
+}
+add_filter( 'json_authentication_errors', 'json_basic_auth_error' );
