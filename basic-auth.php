4ddbb3a1484767102726d87181299da339a18f48
Graham May <graham.may@soapbox.co.uk>
2016-03-21 11:25:21 +0000

    Formatting changed to match original repo

--- basic-auth.php
+++ basic-auth.php
@@ -8,17 +8,15 @@
  * Plugin URI: https://github.com/WP-API/Basic-Auth
  */
 
-function json_basic_auth_handler($user)
-{
+function json_basic_auth_handler( $user ) {
+	global $wp_json_basic_auth_error;
 
-    global $wp_json_basic_auth_error;
+	$wp_json_basic_auth_error = null;
 
-    $wp_json_basic_auth_error = null;
-
-    // Don't authenticate twice
-    if (!empty($user)) {
-        return $user;
-    }
+	// Don't authenticate twice
+	if ( ! empty( $user ) ) {
+		return $user;
+	}
 
     if (!isset($_SERVER['PHP_AUTH_USER']) && (isset($_SERVER['HTTP_AUTHORIZATION']) || isset($_SERVER['REDIRECT_HTTP_AUTHORIZATION']))) {
         if (isset($_SERVER['HTTP_AUTHORIZATION'])) {
@@ -29,51 +27,46 @@ function json_basic_auth_handler($user)
 
         list($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW']) = explode(':', base64_decode(substr($header, 6)));
     }
+    
+	// Check that we're trying to authenticate
+	if ( !isset( $_SERVER['PHP_AUTH_USER'] ) ) {
+		return $user;
+	}
 
-    // Check that we're trying to authenticate
-    if (!isset($_SERVER['PHP_AUTH_USER'])) {
-        return $user;
-    }
+	$username = $_SERVER['PHP_AUTH_USER'];
+	$password = $_SERVER['PHP_AUTH_PW'];
 
-    $username = $_SERVER['PHP_AUTH_USER'];
-    $password = $_SERVER['PHP_AUTH_PW'];
+	/**
+	 * In multi-site, wp_authenticate_spam_check filter is run on authentication. This filter calls
+	 * get_currentuserinfo which in turn calls the determine_current_user filter. This leads to infinite
+	 * recursion and a stack overflow unless the current function is removed from the determine_current_user
+	 * filter during authentication.
+	 */
+	remove_filter( 'determine_current_user', 'json_basic_auth_handler', 20 );
 
-    /**
-     * In multi-site, wp_authenticate_spam_check filter is run on authentication. This filter calls
-     * get_currentuserinfo which in turn calls the determine_current_user filter. This leads to infinite
-     * recursion and a stack overflow unless the current function is removed from the determine_current_user
-     * filter during authentication.
-     */
-    remove_filter('determine_current_user', 'json_basic_auth_handler', 20);
+	$user = wp_authenticate( $username, $password );
 
-    $user = wp_authenticate($username, $password);
+	add_filter( 'determine_current_user', 'json_basic_auth_handler', 20 );
 
-    add_filter('determine_current_user', 'json_basic_auth_handler', 20);
+	if ( is_wp_error( $user ) ) {
+		$wp_json_basic_auth_error = $user;
+		return null;
+	}
 
-    if (is_wp_error($user)) {
-        $wp_json_basic_auth_error = $user;
+	$wp_json_basic_auth_error = true;
 
-        return null;
-    }
-
-    $wp_json_basic_auth_error = true;
-
-    return $user->ID;
+	return $user->ID;
 }
+add_filter( 'determine_current_user', 'json_basic_auth_handler', 20 );
 
-add_filter('determine_current_user', 'json_basic_auth_handler', 20);
+function json_basic_auth_error( $error ) {
+	// Passthrough other errors
+	if ( ! empty( $error ) ) {
+		return $error;
+	}
 
-function json_basic_auth_error($error)
-{
+	global $wp_json_basic_auth_error;
 
-    // Passthrough other errors
-    if (!empty($error)) {
-        return $error;
-    }
-
-    global $wp_json_basic_auth_error;
-
-    return $wp_json_basic_auth_error;
+	return $wp_json_basic_auth_error;
 }
-
-add_filter('json_authentication_errors', 'json_basic_auth_error');
+add_filter( 'json_authentication_errors', 'json_basic_auth_error' );
